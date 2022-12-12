59dcecd55bfda0de42eb29827f45e6c90e400efe
AMEER SHAHUL HAMEED KOTTA <53536783+AMEERKOTTA@users.noreply.github.com>
2022-12-12 14:49:37 +0530

    content_for_pantry_and_recipes

---
+++ Section 7 - Dictionaries and Sets/contents.py
@@ -0,0 +1,65 @@
+pantry = {
+    "chicken": 500,
+    "lemon": 2,
+    "cumin": 24,
+    "paprika": 18,
+    "chilli powder": 7,
+    "yogurt": 300,
+    "oil": 450,
+    "onion": 5,
+    "garlic": 9,
+    "ginger": 2,
+    "tomato puree": 125,
+    "almonds": 75,
+    "rice": 500,
+    "coriander": 20,
+    "lime": 3,
+    "pepper": 8,
+    "egg": 6,
+    "pizza": 2,
+    "spam": 1,
+}
+
+recipes = {
+    "Butter chicken": [
+        "chicken",
+        "lemon",
+        "cumin",
+        "paprika",
+        "chilli powder",
+        "yogurt",
+        "oil",
+        "onion",
+        "garlic",
+        "ginger",
+        "tomato puree",
+        "almonds",
+        "rice",
+        "coriander",
+        "lime",
+    ],
+    "Chicken and chips": [
+        "chicken",
+        "potatoes",
+        "salt",
+        "malt vinegar",
+    ],
+    "Pizza": [
+        "pizza",
+    ],
+    "Egg sandwich": [
+        "egg",
+        "bread",
+        "butter",
+    ],
+    "Beans on toast": [
+        "beans",
+        "bread",
+    ],
+    "Spam a la tin": [
+        "spam",
+        "tin opener",
+        "spoon",
+    ],
+}
+
